ebb5f3d8acd4ce0e0d692c6f720f59f10c7378b6
arsal656 <72801757+arsal656@users.noreply.github.com>
2020-12-03 18:19:32 +0500

    Add files via upload

---
+++ hardware/hardware.ino
@@ -0,0 +1,20 @@
+char a;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT); 
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available())
+  a=Serial.read();
+  if(a==1)
+  {digitalWrite(13,HIGH);
+  delay(100);}
+  else if (a==2)
+  {digitalWrite(13,LOW);
+  delay(100);}
+
+}
